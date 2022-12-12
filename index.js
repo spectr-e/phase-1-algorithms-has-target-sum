c21746d459f3185a568bd3320b4538fba5b32a61
josiah <devs.josia@gmail.com>
2022-12-12 18:38:17 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const len = array.length;
+  let sum;
+  for (pos in array) {
+    for (i = 0; i < len; i++) {
+      if (array[pos] != array[i]) {
+        sum = array[pos] + array[i];
+        if (sum === target) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
 */
 
-/* 
+/*
   Add your pseudocode here
+  declare a function that takes in an array of numbers and an target integer
+  as an arguement 
+  this function should return true if any pair of the numbers in the array sums up to   the target and false if none do
 */
 
 /*
   Add written explanation of your solution here
+  declared the function hasTargetSum() taking in two args; ta, declared one variable and assigned it the length of the array and initialized another to hold the sum of the pairs of the array's integers
+  iterated through the array and added each element to the next one, and (or) previous one and assigned the result to the sum variable
+  compared the sum to target, return two if they are equal and false when the array's elements have been traversed and added to every other.
 */
 
 // You can run `node index.js` to view these console logs
